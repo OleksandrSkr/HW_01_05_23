c32505bc4932a9c65869e1cc41db32114ca9f4d2
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-04-23 22:44:51 +0300

    added by and keys

--- evn/App.py
+++ evn/App.py
@@ -9,7 +9,13 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+import time
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 driver.get("https://www.rozetka.com.ua")
+
